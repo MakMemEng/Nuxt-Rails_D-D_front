8e8a780446b2cc79a0fffa10a711da8f196e25bf
makmemeng <yatani.mems4568@gmail.com>
2021-11-23 15:44:47 +0900

    Add middleware get current project

--- store/index.js
+++ store/index.js
@@ -23,6 +23,19 @@ export const state = () => ({
 
 export const getters = {}
 
-export const mutations = {}
+export const mutations = {
+  setCurrentProject (state, payload) {
+    state.project.current = payload
+  }
+}
 
-export const actions = {}
+export const actions = {
+  // { state, getters, commit, dispatch, rootState, rootGetters }
+  // rootState => ルート(store/index.js)のstateを取得(rootState = state)
+  getCurrentProject ({ state, commit }, params) {
+    const id = Number(params.id)
+    const currentProject =
+      state.project.list.find(project => project.id === id) || null
+    commit('setCurrentProject', currentProject)
+  }
+}
